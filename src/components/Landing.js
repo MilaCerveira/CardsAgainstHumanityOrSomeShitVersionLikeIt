58ab1ad793850145b7570cf3f33860abe59b6655
user <user@users-MBP.broadband>
2022-01-29 00:28:34 +0000

    card

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,18 @@
 const Landing = () => {
-return(
-<h1>I'm a landing page</h1>
-)
+    return (
+        <>
+            <h1>Cards</h1>
+            <h1>Against</h1>
+            <h1> Humanity</h1>
+            <div className='card'>
+                <div className='card-inner'>
+                    <div className='card-front'>
+                        <h1>Instead of coal Santa now gives bad children</h1>
+                    </div>
+                    <div className='card-back'> <h1>Multiple stab wounds</h1></div>
+                </div>
+            </div>
+        </>
+    )
 }
 export default Landing;
