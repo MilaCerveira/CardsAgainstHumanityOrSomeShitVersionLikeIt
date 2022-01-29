9d8fcd78b5fe264b0745f2361964facd8d2a9c07
user <user@users-MBP.broadband>
2022-01-29 16:13:38 +0000

    animate scale on load

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,7 +4,6 @@ const Landing = () => {
     return (
         <>
             <h1>Cards</h1>
-
            <span className='red'><h1>Against</h1></span> 
             <h1> Humanity</h1>
             <div className='card'>
