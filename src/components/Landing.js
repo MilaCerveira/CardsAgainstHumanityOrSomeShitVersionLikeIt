b561b85200c1632a27db8bfd8c6989436a6cee83
user <user@users-MBP.broadband>
2022-01-29 02:04:22 +0000

    bottom of card img

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,5 @@
 import './Landing.css';
-
+import cad from '../assets/cad.png';
 const Landing = () => {
     return (
         <>
@@ -9,11 +9,19 @@ const Landing = () => {
             <div className='card'>
                 <div className='card-inner'>
                     <div className='card-front'>
-                        <h1>Instead of coal Santa now gives bad children....</h1>
+                        <h1>Instead of coal Santa now gives bad children</h1>
+                        <div id="bottom">
+                            <img className='bottom-img' src={cad}/>
+                            Cards Against Humanity
+                        </div>
                     </div>
                     <div className='card-back'>
                         <h1>Multiple stab wounds</h1>
                         <button className="enter-button">ENTER</button>
+                        <div id="bottom">
+                            <img className='bottom-img' src={cad}/>
+                            Cards Against Humanity
+                        </div>
                     </div>
                 </div>
             </div>
