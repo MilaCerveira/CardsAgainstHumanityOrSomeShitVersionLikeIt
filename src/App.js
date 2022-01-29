9b23055227c68fa8333d4ac8ab34bd9d36cfe1f6
user <user@users-MBP.broadband>
2022-01-29 00:35:13 +0000

    card flip on hover animate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
 import './App.css';
 import Landing from './components/Landing';
 
 function App() {
   return (
     <div className="App">
-      <h2> hello</h2>
       <Landing/>
     </div>
   );
