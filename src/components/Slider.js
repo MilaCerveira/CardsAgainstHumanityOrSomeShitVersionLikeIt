8da9e537ad3078eb1a099ae321b1e2dcaa2a00d5
user <user@users-MBP.broadband>
2022-02-02 20:22:13 +0000

    begin slider

---
+++ src/components/Slider.js
@@ -0,0 +1,12 @@
+import { motion } from "framer-motion";
+import { useRef, useEffec, useState } from "react"
+
+const Slider = () => {
+    return (
+        <>
+            <h1>I'm a slider </h1>
+
+        </>
+    )
+}
+export default Slider;
