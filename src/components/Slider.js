13a672f25150450315518f628dec5dca0e6fad6f
user <user@users-MBP.broadband>
2022-02-02 20:25:06 +0000

    framer

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,10 +1,18 @@
 import { motion } from "framer-motion";
-import { useRef, useEffec, useState } from "react"
+import { useRef, useEffect, useState } from "react";
+import white from '../assets/white.png';
+import black from '../assets/black.png';
 
 const Slider = () => {
     return (
         <>
             <h1>I'm a slider </h1>
+            <motion.div className="slider">
+                <motion.div className="inner-slider">
+
+                </motion.div>
+
+            </motion.div>
 
         </>
     )
