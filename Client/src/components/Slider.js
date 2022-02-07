37bff01790803b41b7f9298492d9a5641597c55e
user <user@users-MBP.broadband>
2022-02-07 11:53:37 +0000

    deleting unused items

--- Client/src/components/Slider.js
+++ Client/src/components/Slider.js
@@ -1,7 +1,6 @@
 import { useRef, useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import './Slider.css';
-import images from "./images";
 import WhiteCard from "./WhiteCard";
 
 const Slider = ({ hand }) => {
