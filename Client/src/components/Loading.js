c1e305169c3331341fd351552c17cd031b2d81d7
user <user@users-MBP.broadband>
2022-02-17 14:03:05 +0000

    loading.js

---
+++ Client/src/components/Loading.js
@@ -0,0 +1,9 @@
+import load from '../assets/load.GIF';
+
+const Loading = () => {
+  return (
+    <img src={load} alt='loading' style={{width: '200px', margin:'auto', display: 'block'}}/>
+  )
+}
+
+export default Loading
