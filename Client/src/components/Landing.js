ec51e20d1e34902b549385c2d81266de0cc8b2c8
user <user@users-MBP.broadband>
2022-02-18 10:10:56 +0000

    losing screen

--- Client/src/components/Landing.js
+++ Client/src/components/Landing.js
@@ -5,12 +5,12 @@ import Modal from "./Modal";
 import Loser from "./Loser";
 
 
+
 const Landing = () => {
     const [showModal, setShowModal] = useState(false);
 
     return (
         <>
-        <Loser/>
             <h1>Cards</h1>
             <span className='red'><h1>Against</h1></span>
             <h1> Humanity</h1>
@@ -34,7 +34,7 @@ const Landing = () => {
                     </div>
                 </div>
             </div>
-
+<Loser/>
         </>
     )
 }
--- Client/src/components/Loser.js
+++ Client/src/components/Loser.js
@@ -11,10 +11,22 @@ const Loser = () => {
 
     const emoArray = [];
     for (let i = 0; i <= 100 ; i++) {
-      emoArray.push( ' 💩 ')
+      emoArray.push( '💩')
       emoArray.push('👎  ')
       emoArray.push('🗑️')
       emoArray.push ('🐀')
+      emoArray.push('🍅 ')
+      emoArray.push('Loser')
+    }
+
+    const emoArray2 = [];
+    for (let i = 0; i <= 100 ; i++) {
+      emoArray2.push( '💩')
+      emoArray2.push('👎  ')
+      emoArray2.push('🗑️')
+      emoArray2.push ('🐀')
+      emoArray2.push('🍅 ')
+      emoArray2.push('Loser')
     }
    const setEmoji = emoArray.map((emo) => {
         return (
@@ -45,11 +57,45 @@ const Loser = () => {
         );
     })
 
+    const setEmoji2 = emoArray2.map((emo) => {
+        return (
+            <motion.div 
+            initial={{ y: Math.random() * -800 , x: Math.random() * -800 }}
+            animate={{
+               
+             
+              y: Math.random() * -600,
+              x: Math.random() * -600,
+              scale: [2, 3, 2, 3, 2]
+            }}
+            transition={{
+              duration: 5,
+              ease: "easeInOut",
+              loop: Infinity,
+              repeatDelay: 2
+            }}
+            whileHover={{ scale: 35 }}
+            whileTap={{ rotate: [0, 5, -5, 5, 0] }}
+          >
+          
+            <span role="img" aria-label="poop">
+              {emo}
+            </span>
+          
+          </motion.div>
+        );
+    })
+
   return (
     <div className='background'>
       <motion.div 
      >
      {setEmoji}
+     
+     </motion.div>
+     <motion.div 
+     >
+     {setEmoji2}
      
      </motion.div>
     <button onClick={play}>Loser</button>
