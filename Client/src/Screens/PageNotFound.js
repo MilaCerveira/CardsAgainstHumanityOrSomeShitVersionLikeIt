d4a5f489afc9d3ff264c13ff6e5f7dcbf8a460d6
user <user@users-MBP.broadband>
2022-02-23 13:20:18 +0000

    error page

--- Client/src/Screens/PageNotFound.js
+++ Client/src/Screens/PageNotFound.js
@@ -1,10 +1,14 @@
-
+import oddjob from '../assets/oddjob.jpeg';
+import n64 from '../assets/n64.png';
 
 const PageNotFound = () => {
 
     return(
         <>
             <h2>404 Error</h2>
+
+            <img src={oddjob} alt='oddjob' />
+            <img  src={n64} alt='Boris' />
         </>
     )
 }
